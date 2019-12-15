2ca7aa9608dad7c663158cc6d67a26fa478cf581
MICHAEL HUND <mahund@uc.cl>
2019-12-15 12:32:31 -0300

    feat(chat): ModeratorId as string

---
+++ Migrations/20191215152435_ModeratorTypeChange.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcMovie.Migrations
+{
+    public partial class ModeratorTypeChange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ModeratorId",
+                table: "Chat",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ModeratorId",
+                table: "Chat",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
